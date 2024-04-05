fc1611a57f8988009336b72b0d9f72ff20dca46b
ERP-FTW <124227412+ERP-FTW@users.noreply.github.com>
2024-04-04 21:02:42 -0400

    Update __manifest__.py
    removed missing data file

--- mlr_ecommerce_btcpay_2/__manifest__.py
+++ mlr_ecommerce_btcpay_2/__manifest__.py
@@ -8,7 +8,6 @@
     "depends": ["website", "mlr_ecommerce_cryptopayments"],
     "data": [
         "views/btcpay_payment_template.xml",
-        "data/btcpay_payment_method_data.xml",  
         "data/btcpay_payment_provider_data.xml",
         "views/btcpay_payment_form.xml",
         "views/btcpay_payment_provider.xml",
