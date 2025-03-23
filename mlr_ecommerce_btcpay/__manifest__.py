f07502ba883562253d1755297196da9e51f71948
ERP-FTW <124227412+ERP-FTW@users.noreply.github.com>
2025-03-22 20:48:21 -0400

    Update __manifest__.py

--- mlr_ecommerce_btcpay/__manifest__.py
+++ mlr_ecommerce_btcpay/__manifest__.py
@@ -10,6 +10,7 @@
     "data": [
         "views/btcpay_payment_template.xml",
         "data/btcpay_payment_provider_data.xml",
+        "data/btcpay_payment_method_data.xml",
         "views/btcpay_payment_form.xml",
         "views/btcpay_payment_provider.xml",
 
